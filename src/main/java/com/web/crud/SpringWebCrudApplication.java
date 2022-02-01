21b5a4dcdaadccc7c06f323fb5190faf83c04445
Rishabh Sharma <rish.sharma9012@gmail.com>
2022-02-01 12:16:35 +0530

    commit

--- src/main/java/com/web/crud/SpringWebCrudApplication.java
+++ src/main/java/com/web/crud/SpringWebCrudApplication.java
@@ -2,12 +2,22 @@ package com.web.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+import com.web.repo.StudentRepository;
+
+
+@ComponentScan(basePackages = "com.web.controller")
+@ComponentScan(basePackages = "com.web.repo")
+@SpringBootApplication(scanBasePackages={
+"com.web.controller", "com.web.repo"})
+@EnableMongoRepositories(basePackageClasses =StudentRepository.class)
 public class SpringWebCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebCrudApplication.class, args);
+		System.out.println("hello rishabh");
 	}
 
 }
